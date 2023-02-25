6bbeec708eac79769b44faea578d06d7074396b1
rajshekar11 <rajshekar767@gmail.com>
2023-02-25 19:34:18 +0530

    updated

--- Employee_app/src/main/java/com/masai/EmployeeAppApplication.java
+++ Employee_app/src/main/java/com/masai/EmployeeAppApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 
-//@OpenAPIDefinition( 
-//	    servers = {
-//	       @Server(url = "/", description = "Default Server URL")
-//	    }
-//	) 
+@OpenAPIDefinition( 
+	    servers = {
+	       @Server(url = "/", description = "Default Server URL")
+	    }
+	) 
 
 @SpringBootApplication
 public class EmployeeAppApplication {
